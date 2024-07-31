203e6029a3ff7645dab0edca8cd1f912f9cf7070
dieghernan <diego.hernangomezherrero@gmail.com>
2024-07-31 12:37:27 +0000

    Update function and add aemet_alert_zones

---
+++ tests/testthat/_snaps/aemet_alert_zones.md
@@ -0,0 +1,16 @@
+# Errors and validations
+
+    Code
+      aemet_alert_zones(return_sf = "A")
+    Condition
+      Error in `aemet_alert_zones()`:
+      ! is.logical(return_sf) is not TRUE
+
+---
+
+    Code
+      aemet_alert_zones(verbose = "A")
+    Condition
+      Error in `aemet_alert_zones()`:
+      ! is.logical(verbose) is not TRUE
+
--- tests/testthat/test-aemet_alerts.R
+++ tests/testthat/test-aemet_alerts.R
@@ -7,6 +7,16 @@ test_that("Errors", {
   expect_snapshot(aemet_alerts(lang = "frr"), error = TRUE)
 })
 
+test_that("Metadata", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_if_not(aemet_detect_api_key(), message = "No API KEY")
+
+  meta <- aemet_alerts(st, extract_metadata = TRUE)
+  # Same as
+  meta2 <- aemet_alerts("NOEXIST", extract_metadata = TRUE)
+  expect_identical(meta, meta2)
+})
 
 test_that("In no alerts", {
   skip_on_cran()
@@ -15,12 +25,12 @@ test_that("In no alerts", {
 
   df <- aemet_hlp_alerts_master()
 
-  seehere <- sort(unique(df$aemet))
+  seehere <- sort(unique(df$COD_CCAA))
 
   # Check if there is any CCAA without alert to perform test
 
   df_map <- ccaa_to_aemet()
-  df_map <- df_map[!df_map$aemet %in% seehere, ]
+  df_map <- df_map[!df_map$COD_CCAA %in% seehere, ]
 
   skip_if(nrow(df_map) == 0,
     message = "All CCAA with alerts, can't perform test"
@@ -41,12 +51,12 @@ test_that("In alerts", {
 
   df <- aemet_hlp_alerts_master()
 
-  seehere <- sort(unique(df$aemet))
+  seehere <- sort(unique(df$COD_CCAA))
 
   # Check if there is any CCAA with alert to perform test
 
   df_map <- ccaa_to_aemet()
-  df_map <- df_map[df_map$aemet %in% seehere, ]
+  df_map <- df_map[df_map$COD_CCAA %in% seehere, ]
 
   skip_if(nrow(df_map) == 0,
     message = "All CCAA without alerts, can't perform test"
@@ -56,8 +66,13 @@ test_that("In alerts", {
 
   ca <- mapSpain::esp_dict_region_code(smp, "codauto")
 
-  expect_message(res <- aemet_alerts(ca, verbose = TRUE))
+  expect_message(res <- aemet_alerts(ca, verbose = TRUE, return_sf = TRUE))
   expect_s3_class(res, c("sf", "tbl_df", "tbl", "data.frame"), exact = TRUE)
+
+  # No sf
+  res_tbl <- aemet_alerts(ca, return_sf = FALSE)
+
+  expect_identical(sf::st_drop_geometry(res), res_tbl)
 
   # Now lang
   res2 <- aemet_alerts(ca, verbose = TRUE, lang = "en")
