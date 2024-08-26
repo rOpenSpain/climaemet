2a446b36a090e431b1f227df092e4df357a0ea6a
Diego H <diego.hernangomezherrero@gmail.com>
2024-08-26 21:24:52 +0200

    Add docs

--- R/aemet_forecast_fires.R
+++ R/aemet_forecast_fires.R
@@ -1,10 +1,86 @@
-aemet_forecast_fires <- function(area = c("p", "c"), verbose = FALSE) {
+#' AEMET fires forecast
+#'
+#' @description
+#'
+#' Get a [`SpatRaster`][terra::rast()] as provided by \CRANpkg{terra} with the
+#' daily meteorological risk level for wildfires.
+#'
+#' @family aemet_api_data
+#' @family forecasts
+#'
+#' @param area The area, being:
+#'   - `"p"` for Mainland Spain and Balearic Islands.
+#'   - `"c"` for Canary Islands.
+#' @inheritParams get_data_aemet
+#' @inheritParams aemet_daily
+#'
+#' @source
+#'
+#' <https://www.aemet.es/en/eltiempo/prediccion/incendios>.
+#'
+#' @return A [`tibble`][tibble::tibble()] or a [`SpatRaster`][terra::rast()]
+#' object.
+#'
+#' @details
+#' The `SpatRaster` provides 5 [factor()] levels with the following meaning:
+#'   - `"1"`: Low risk.
+#'   - `"2"`: Moderate risk.
+#'   - `"3"`: High risk.
+#'   - `"4"`: Very high risk.
+#'   - `"5"`: Extreme risk.
+#'
+#' The resulting object has several layers, each one representing the forecast
+#' for the upcoming 7 days. It also has additional attributes provided by the
+#' \CRANpkg{terra} package, such as [terra::time()] and [terra::coltab()].
+#'
+#'
+#'
+#' @export
+#'
+#' @examplesIf aemet_detect_api_key()
+#' aemet_forecast_fires(extract_metadata = TRUE)
+#'
+#' # Extract alerts
+#' alerts <- aemet_forecast_fires()
+#'
+#' alerts
+#'
+#' # Nice plotting
+#' library(ggplot2)
+#' library(tidyterra)
+#'
+#' ggplot() +
+#'   geom_spatraster(data = alerts) +
+#'   facet_wrap(~lyr, nrow = 2)
+#'
+#' # Zoom in an area
+#' cyl <- mapSpain::esp_get_ccaa("Castilla y Leon", epsg = 4326)
+#'
+#' fires_cyl <- terra::crop(alerts, cyl)
+#'
+#' ggplot() +
+#'   geom_spatraster(data = fires_cyl) +
+#'   geom_sf(data = cyl, fill = NA, color = "black", linewidth = 0.5) +
+#'   facet_wrap(~lyr, nrow = 2)
+#'
+#' @export
+aemet_forecast_fires <- function(area = c("p", "c"), verbose = FALSE,
+                                 extract_metadata = FALSE) {
   # 1. Validate inputs----
   area <- match.arg(area)
   stopifnot(is.logical(verbose))
   verb <- ifelse(verbose, 3, 0)
 
-  # 2. Download from feed ----
+
+
+  # 2. Download ----
+
+  ## Metadata ----
+  if (extract_metadata) {
+    apidest <- "/api/incendios/mapasriesgo/previsto/dia/1/area/p"
+    final_result <- get_metadata_aemet(apidest = apidest, verbose = verbose)
+    return(final_result)
+  }
 
   feed_url <- "https://www.aemet.es/es/api-eltiempo/incendios/download"
 
