9618e7c2cacfd7c627c8e501d884fd361c040c3e
Diego H. <diego.hernangomezherrero@gmail.com>
2026-07-10 22:44:51 +0200

    Use temporary PDF device in tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,13 @@
 # Testing only
 
+test_env <- testthat::teardown_env()
+
 withr::local_options(
   list(climaemet_timeout = 300),
-  .local_envir = testthat::teardown_env()
+  .local_envir = test_env
+)
+
+withr::local_pdf(
+  withr::local_tempfile(fileext = ".pdf", .local_envir = test_env),
+  .local_envir = test_env
 )
